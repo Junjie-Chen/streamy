305f7f24da0d0c8b0651451b36cdbd661de0359e
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 11:57:19 -0500

    feat(component): render stream list in browser

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class StreamList extends Component {
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera"></i>
+          <div className="content">
+            <Link className="header" to={`/streams/${stream.id}`}>{stream.title}</Link>
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
         <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
       </div>
     );
   }
