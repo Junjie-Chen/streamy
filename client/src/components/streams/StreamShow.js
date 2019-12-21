d217cded690b722af436e4b2c66aa573529ac5ab
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 12:18:21 -0500

    feat(component): render loading state conditionally

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 class StreamShow extends Component {
   render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
     return <div>Stream Show</div>;
   }
 }
