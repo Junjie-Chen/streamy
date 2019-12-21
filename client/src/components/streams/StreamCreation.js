7faf968d6c90028be571080a20bbc70054071bfd
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 13:17:13 -0500

    feat(component): render stream creation form in browser

--- client/src/components/streams/StreamCreation.js
+++ client/src/components/streams/StreamCreation.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import StreamForm from './StreamForm';
 
 class StreamCreation extends Component {
   render() {
     return (
       <div>
         <h3>Create a Stream</h3>
+        <StreamForm />
       </div>
     );
   }
