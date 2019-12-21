8e674a443624bead0d08139d688e38840bc60154
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 13:48:05 -0500

    feat(component): add onClick event and its handler to modal background

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
+    <div className="ui dimmer modals visible active" onClick={props.onDismiss}>
       <div className="ui standard modal visible active" onClick={event => event.stopPropagation()}>
         <div className="header">{props.header}</div>
         <div className="content">{props.content}</div>
