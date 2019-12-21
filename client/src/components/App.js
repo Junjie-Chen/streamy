28d6495c8c1f7665dd3edea9ab641f6bdb9193a2
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 14:01:19 -0500

    feat(router): use Router component from React Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 import Header from './Header';
 
 const App = () => {
   return (
     <div className="ui container">
-      <Header />
+      <Router>
+        <Header />
+      </Router>
     </div>
   );
 };
