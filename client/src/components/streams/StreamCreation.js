d76b5c5c97b84b6d25152897db41fd6e2ec4f20c
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 13:17:57 -0500

    feat(component): add onSubmit render prop to form

--- client/src/components/streams/StreamCreation.js
+++ client/src/components/streams/StreamCreation.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import StreamForm from './StreamForm';
 
 class StreamCreation extends Component {
+  onSubmit = () => {};
+
   render() {
     return (
       <div>
         <h3>Create a Stream</h3>
-        <StreamForm />
+        <StreamForm onSubmit={this.onSubmit} />
       </div>
     );
   }
