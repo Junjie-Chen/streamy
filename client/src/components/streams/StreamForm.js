00a712032047018ed85a06d6419f08784806223a
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 12:58:47 -0500

    feat(component): render title field in form

--- client/src/components/streams/StreamForm.js
+++ client/src/components/streams/StreamForm.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { Field } from 'redux-form';
 
 class StreamForm extends Component {
+  renderInput = inputProps => {
+    return (
+      <div className="field">
+        <label>{inputProps.label}</label>
+        <input value={inputProps.input.value} onChange={inputProps.input.onChange} autoComplete="off" />
+      </div>
+    );
+  };
+
   render() {
-    return <form className="ui form error"></form>;
+    return (
+      <form className="ui form error">
+        <Field name="title" label="Enter Title" component={this.renderInput} />
+      </form>
+    );
   }
 }
 
