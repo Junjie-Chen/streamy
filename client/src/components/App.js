146ea586854886b60ae5436b64bad507e3ad6b0c
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 11:50:46 -0500

    feat(component): render header in application

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './Header';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
