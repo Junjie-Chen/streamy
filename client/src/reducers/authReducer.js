795d0178f4c4a8db48302e8673ed83f42a38179b
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 11:45:51 -0500

    feat(reducer): update auth state with sign-out action

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,4 +1,4 @@
-import { SIGN_IN } from '../actions/types';
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
 const INITIAL_STATE = {
   isSignedIn: null,
@@ -9,6 +9,8 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_IN:
       return { ...state, isSignedIn: true, userId: action.payload };
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false, userId: null };
     default:
       return state;
   }
