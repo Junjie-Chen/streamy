318aeae8297cf51ed7bddd0237d2d72e9745fd43
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 13:25:04 -0500

    feat(reducer): update streams state with stream creation action

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
-import { FETCH_STREAMS, FETCH_STREAM } from '../actions/types';
+import { FETCH_STREAMS, FETCH_STREAM, CREATE_STREAM } from '../actions/types';
 
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAMS:
       return { ...state, ..._.mapKeys(action.payload, 'id') };
     case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     default:
       return state;
