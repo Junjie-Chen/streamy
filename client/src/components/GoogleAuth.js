3e4ca79c9dcfe8fab2de4d0ac0c232bf8b7881f0
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 10:52:14 -0500

    feat(component): load client and auth2 libraries from Google API inside componentDidMount lifecycle method

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {});
+  }
+
   render() {
     return <div>Google Auth</div>;
   }
