edda5e04bda5f0848b04efae9fcbbb7be13cd68d
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 13:39:25 -0500

    feat(type): create EDIT_STREAM variable

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,3 +3,4 @@ export const SIGN_OUT = 'SIGN_OUT';
 export const FETCH_STREAMS = 'FETCH_STREAMS';
 export const FETCH_STREAM = 'FETCH_STREAM';
 export const CREATE_STREAM = 'CREATE_STREAM';
+export const EDIT_STREAM = 'EDIT_STREAM';
