7e1c2005f063379ec49af7efd15b7b6afc1cecef
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 13:47:02 -0500

    feat(component): render modal in browser

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Modal = () => {
+const Modal = props => {
   return ReactDOM.createPortal(
+    <div className="ui dimmer modals visible active">
+      <div className="ui standard modal visible active">
+        <div className="header">{props.header}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
+      </div>
+    </div>,
     document.getElementById('modal')
   );
 };
