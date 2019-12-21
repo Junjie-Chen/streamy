82d5b85f059e03571cf8da4113ec58ce59e63c8e
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 11:39:50 -0500

    feat(store): provide state to all components

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers());
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
