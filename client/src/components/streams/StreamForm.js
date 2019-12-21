88ba42875c5e6e76d5a9862f9f8da24bf3e9a1ba
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 13:06:20 -0500

    feat(component): render error messages in browser

--- client/src/components/streams/StreamForm.js
+++ client/src/components/streams/StreamForm.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import { Field } from 'redux-form';
 
 class StreamForm extends Component {
-  renderInput = ({ input, label }) => {
+  renderError(meta) {
+    if (meta.error && meta.touched) {
+      return (
+        <div className="ui error message">
+          <div className="header">{meta.error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
         <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
   };
