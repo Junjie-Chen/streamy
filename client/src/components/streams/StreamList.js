69555cf6cd248ef7fb703f9685010e58b8d873a1
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 11:58:15 -0500

    feat(component): render edit and delete buttons in browser

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class StreamList extends Component {
+  renderEditAndDeleteButtons(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <Link className="ui button primary" to={`/streams/edit/${stream.id}`}>Edit</Link>
+          <Link className="ui button negative" to={`/streams/delete/${stream.id}`}>Delete</Link>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderEditAndDeleteButtons(stream)}
           <i className="large middle aligned icon camera"></i>
           <div className="content">
             <Link className="header" to={`/streams/${stream.id}`}>{stream.title}</Link>
