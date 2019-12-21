3bb83e582f37faf36c5a7cbc4ca5aeeb3d6dd1e5
Junjie Chen <junjie.chen18@gmail.com>
2019-12-21 13:50:35 -0500

    feat(component): add content prop to modal

--- client/src/components/streams/StreamDeletion.js
+++ client/src/components/streams/StreamDeletion.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import Modal from '../Modal';
 
 class StreamDeletion extends Component {
+  renderContent() {
+    if (!this.props.stream) {
+      return 'Are you sure you want to delete the stream?';
+    }
+
+    return `Are you sure you want to delete the stream: ${this.props.stream.title}?`;
+  }
+
   render() {
-    return <Modal header="Delete the Stream" />;
+    return <Modal header="Delete the Stream" content={this.renderContent()} />;
   }
 }
 
